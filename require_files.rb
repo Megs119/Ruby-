4ce0891964fae80998848508ba6ee8bbc7217931
Megan Devenney <megdevenney@gmail.com>
2018-06-13 16:35:24 +0100

    Add files via upload

---
+++ require_files.rb
@@ -0,0 +1,31 @@
+#Require files 
+
+#load - read and parse file every time you call load
+#load '/Users/Mdevenney/eclipse-workspace/seleniumguru-ruby/types_of_variables.rb'
+
+#file1 = Algebra.new()
+#file1.value
+
+
+#require - read and parse file only once (saves memory)
+#require '/Users/Mdevenney/eclipse-workspace/seleniumguru-ruby/types_of_variables.rb'
+#
+#file1 = Algebra.new()
+#file1.value
+
+
+#require 'Date'
+#c1 = Date.new
+#puts c1.day
+
+require 'Time'
+
+t1 = Time.now
+puts t1
+
+#require_relative - used when file is in the same folder
+#require_relative 'types_of_variables'
+#
+#file1 = Algebra.new()
+#file1.value
+
