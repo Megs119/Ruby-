d7ca118ac66517111a49366e310bd787eb109db8
Megan Devenney <megdevenney@gmail.com>
2018-07-04 13:15:11 +0100

    Update login-class.rb

--- login-class.rb
+++ login-class.rb
@@ -9,6 +9,8 @@ driver = Selenium::WebDriver.for :firefox
 #driver = Selenium::WebDriver.for :firefox
 #driver = Selenium::WebDriver.for :ie
 
+driver.manage.timeouts.implicit_wait = 5
+
 #maximize the window
 driver.manage.window.maximize 
 
@@ -25,7 +27,13 @@ username.send_keys("mdevenney")
 
 
 #find password textbox 
-password = driver.find_element(:name, "di_p")
+#password = driver.find_element(:name, "di_p")
+#password = driver.find_element(:id, "di_p")
+#password = driver.find_element(:css, "input[id='di_p']")
+#password = driver.find_element(:css, "input[id='di_p']")
+#password = driver.find_element(:css, "input#di_p")
+password = driver.find_element(:css, "input[name^='di_p']")
+
 
 #type password in text box
 password.send_keys("J0ttaPad!")
@@ -35,3 +43,15 @@ sign_in_btn = driver.find_element(:xpath, "//*[@id='loginForm']/div/div[2]/butto
 
 #click sign in button 
 sign_in_btn.click
+
+#Validate title of next page
+puts driver.title
+
+#validitate the title
+if driver.title.downcase == "decibel > login"
+  puts "Test Case Passed"
+else
+  raise "Test Case Failed"
+end
+
+driver.quit
